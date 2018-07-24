ef0f572a4263180856dfd16ecf9032735afc2ad4
Ram Chhabra <rlc42847@gmail.com>
2018-07-24 15:09:17 -0700

    Some-addition-comment
    only one comment

--- bookrepwithcart/bookrep/main.py
+++ bookrepwithcart/bookrep/main.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+#Without any doubt if i have knowledge so i will contribute for your further projects 
 import webapp2
 import cgi
 
